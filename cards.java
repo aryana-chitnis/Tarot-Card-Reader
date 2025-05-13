80e0611780b59efecaf1c478040629eb84e5cc04
aryana-chitnis <ac1000@pleasantonusd.net>
2025-05-13 21:35:23 +0000

    wrote a basic cards class constructor intializing  arraylists

---
+++ cards.java
@@ -0,0 +1,17 @@
+public class cards{
+    private Arraylist<String> romanNum;
+    private ArrayList<String> majorArc;
+    private ArrayList<String> suits;
+
+    public cards(){
+
+        romanNum = {"I", "II", "III", "IV", "V", "VI", "VII", "VII", "IX", "X"};
+        majorArc = {"The Fool", "The Magician", "The High Preistess", "The Empress", "The Hierophant", "The Lovers", "The Chariot", "Stength", "The Hermit", "Wheel Of Fortune", "Justice", "The Hanged Man", "Death", "Temperance", "The Devil", "The Tower", "The Star", "The Moon", "The Sun", "Judgement", "The World", "King of Swords", "The Emperor", "Knight of Swords"};
+        suits = {" Wands"," Swords", " Cups", " Pentacles"};
+    }
+
+
+
+
+
+}
