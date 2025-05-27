034dc49244b0518785e3444b819cfc31fef1b442
yichen <yichencai2008@gmail.com>
2025-05-27 04:21:07 +0000

    adding dialogue to main, moving major/upright into pullingcard method

--- Tarot.java
+++ Tarot.java
@@ -1,81 +1,91 @@
-import java.util.Scanner; // for user input
+import java.util.Scanner; // for user input in main
+import java.util.ArrayList; // for arraylists
 
 public class Tarot {
-	private boolean major, upright, numeral, suit;
-	private ArrayList<String> majorList, romanNum, suits;
+	private boolean major, upright;
+	private ArrayList<String> romanNum, majorList, suitList;
 	
-	public Tarot {
+	public Tarot() { // constructor
+        major = true;
+        upright = true;
 
-	int majorMin = (int)(Math.random()*100);
-	int upDown = (int)(Math.random()*2)
-	
-//adding Math.random function to distinguish major versus minor cards
+        // initializes the private instance vars with diff lists describing the cards roman numeral, major arcana names, and suits
+        romanNum = new ArrayList<>();
+        String[] numerals = {"Ace", "I", "II", "III", "IV", "V", "VI", "VII", "VII", "IX", "X", "Page", "Knight", "Queen", "King"};
+        for (String num : numerals) {
+            romanNum.add(num);
+        }
 
-	if (majorMin < 28) {
-		major = true;
-} 
-else {
-	major = false;
-}
+        majorList = new ArrayList<>();
+        String[] majors = {"Fool", "Magician", "High Priestess", "Empress", "Hierophant", "Lovers", "Chariot", "Stength", "Hermit", "Wheel Of Fortune", "Justice", "Hanged Man", "Death", "Temperance", "Devil", "Tower", "Star", "Moon", "Sun", "Judgement", "World", "King of Swords", "Emperor", "Knight of Swords"};
+        for (String maj : majors) {
+            majorList.add(maj);
+        }
 
-// adding Math.random function to tell if card is pulled rightside up or upside down
-
-	if (upDown > 0) {
-		upright = true;
-	} 
-else {
-		upright = false;
+        suitList = new ArrayList<>();
+        String[] suits = {"Wands","Swords", "Cups", "Pentacles"};
+        for (String s : suits) {
+            suitList.add(s);
+        }
 	}
 
-// initializes the private isntance vars with diff lists describing the cards roman numeral, major arcana names, and suits
-romanNum = {"Ace", "I", "II", "III", "IV", "V", "VI", "VII", "VII", "IX", "X", "Page", "Knight", "Queen", "King"};
-       majorList = {"Fool", "Magician", "High Preistess", "Empress", " Hierophant", "Lovers", "Chariot", "Stength", "Hermit", "Wheel Of Fortune", "Justice", "Hanged Man", "Death", "Temperance", "Devil", "Tower", "Star", "Moon", "Sun", "Judgement", "World", "King of Swords", "Emperor", "Knight of Swords"};
-       suits = {"Wands","Swords", "Cups", "Pentacles"};
+	public String pullingCard() {
+        int majorMin = (int)(Math.random() * 100);// [0,99]
+        int upDown = (int)(Math.random() * 2); // [0,1]
+        
+        // adding Math.random function to distinguish major versus minor cards
+        if (majorMin < 28) {
+            major = true;
+        } 
+        else {
+            major = false;
+        }
 
+        // adding Math.random function to tell if card is pulled rightside up or upside down 
+        if (upDown > 0) {
+            upright = true;
+        } 
+        else {
+            upright = false;
+        }
 
+        if (major == true) {
+            //pulls out a random major card
+            int pullMajor = (int)(Math.random() * 23); // [0,22] -> 23 strings in majorList
+            String newCard = majorList.get(pullMajor);
+            majorArcana(newCard); // prints info
+            if (upright) {
+                System.out.println("(Upright)");
+            }
+            else {
+                System.out.println("(Reverse)");
+            }
+            return ""; 
+        } 
+        else {
+            //pulls out a random roman numeral
+            int pullRomanNum = (int)(Math.random() * 15); // [0,14] -> 15 strings in romanNum
+            String newNum = romanNum.get(pullRomanNum);
+            romanNum(newNum);
+
+            // pulls out a random suit
+            int pullSuit = (int)(Math.random() * 4); // [0,3] -> 4 strings in suitList
+            String newSuit = suitList.get(pullSuit);
+            cardSuit(newSuit);
+            if (upright) {
+                System.out.println("(Upright)");
+            }
+            else {
+                System.out.println("(Reverse)");
+            }
+            return ""; 
+        }
 	}
 
-	public String pullingCard(){
+	public void majorArcana(String pulledCard) { 
+        //the conditional below prints out a description based on the card you pull
 
-
-	if (major == true) {
-
-//pulls out a random major card
-		int pullMajor = (int)(Math.random() * 23);
-		String newCard = majorList.get(pullMajor);
-		majorArcana(newCard);
-
-		} else{
-	//pulls out a random roman numeral
-		int pullRomanNum = (int)(Math.random() * 16);
-		String newNum = roman.get(pullRomanNum);
-		romanNum(newNum);
-	// pulls out a random suit
-		int pullSuit = (int)(Math.random() * 5);
-		String newSuit = suits.get(pullSuit);
-		cardSuit(newSuit);
-
-		}
-
-
-
-	}
-
-	public static void main(String args[]) {
-		Scanner input = new Scanner(System.in);
-		
-		pullingCard();
-		pullingCard();
-		pullingCard();
-	}
-
-
-
-	public void majorArcana(pulledCard) { 
-
-	//the conditional below prints out a description based on the card you pull
-
-	if (pulledCard.equals("Fool")) {
+        if (pulledCard.equals("Fool")) {
             System.out.println("You pulled the Fool. This card represents new beginnings, spontaneity, and a free spirit.");
         } else if (pulledCard.equals("Magician")) {
             System.out.println("You pulled the Magician. This card symbolizes manifestation, resourcefulness, and power.");
@@ -121,16 +131,13 @@ romanNum = {"Ace", "I", "II", "III", "IV", "V", "VI", "VII", "VII", "IX", "X", "
             System.out.println("You pulled the World. This card stands for completion, achievement, and fulfillment.");
         } else {
             System.out.println("Card not recognized.");
-        }
-
+        }	
+    }
 	
-}
+    public void romanNum(String num){
+        // the conditional below prints a description for the Card rank you pull
 
-	
-public void romanNum(num){
-
-	// the conditional below prints a description for the Card rank you pull
-if (num.equals("Ace")) {
+        if (num.equals("Ace")) {
             System.out.println("You pulled the Ace. This symbolizes pure potential, beginnings, and the seed of something new.");
         } else if (num.equals("I")) {
             System.out.println("You pulled a Minor Arcana with Roman Numeral I. This symbolizes new beginnings, potential, and a spark of energy.");
@@ -163,27 +170,55 @@ if (num.equals("Ace")) {
         } else {
             System.out.println("Card not recognized.");
         }
-
 	}
 
-public void cardSuit(pulledSuit){
+    public void cardSuit(String suit){
 
-//the conditionals below describe the suit behavior of the pulled card
-  if (suit.equals("Wands")) {
-            System.out.println("You pulled a Minor Arcana card from the suit of Wands.");
-            System.out.println("Wands represent creativity, passion, inspiration, ambition, and taking action.");
+        //the conditionals below describe the suit behavior of the pulled card
+        if (suit.equals("Wands")) {
+            System.out.print("You pulled a Minor Arcana card from the suit of Wands.");
+            System.out.println(" Wands represent creativity, passion, inspiration, ambition, and taking action.");
         } else if (suit.equals("Cups")) {
-            System.out.println("You pulled a Minor Arcana card from the suit of Cups.");
-            System.out.println("Cups represent emotions, intuition, relationships, and the subconscious.");
+            System.out.print("You pulled a Minor Arcana card from the suit of Cups.");
+            System.out.println(" Cups represent emotions, intuition, relationships, and the subconscious.");
         } else if (suit.equals("Swords")) {
-            System.out.println("You pulled a Minor Arcana card from the suit of Swords.");
-            System.out.println("Swords represent intellect, communication, truth, conflict, and decisions.");
+            System.out.print("You pulled a Minor Arcana card from the suit of Swords.");
+            System.out.println(" Swords represent intellect, communication, truth, conflict, and decisions.");
         } else if (suit.equals("Pentacles")) {
-            System.out.println("You pulled a Minor Arcana card from the suit of Pentacles.");
-            System.out.println("Pentacles represent the material world: finances, work, health, and physical reality.");
+            System.out.print("You pulled a Minor Arcana card from the suit of Pentacles.");
+            System.out.println(" Pentacles represent the material world: finances, work, health, and physical reality.");
         } else {
             System.out.println("Suit not recognized.");
         }
+    }
 
-}
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the Tarot Card Reader! Before we begin, what is your name?");
+        String name = input.nextLine();
+
+        System.out.println("Are you familiar with Tarot, " + name + "? (Yes/No)");
+        String familiar = input.nextLine();
+        if (familiar.toLowerCase().equals("yes") || familiar.toLowerCase().equals("y")) {
+            System.out.println("Great! In this Tarot reading, we will be drawing 3 standard Tarot cards.");
+        }
+        else {
+            System.out.println("Tarot is a system of 78 cards used to for divination and fortune telling. The cards are split into Major Arcana (broad, significant experiences) and Minor Arcana (detailed, immediate experiences). Minor Arcana is further organized into roman numerals and suits. Both types have a chance to be reversed, meaning its qualities are diminished, undeveloped, or blocked.");
+        }
+
+        System.out.println("Now, we shall begin. Let us look into your future, " + name + "...");
+
+        Tarot tarot = new Tarot();
+
+        System.out.print("First, ");
+        tarot.pullingCard();
+
+        System.out.print("Second, ");
+        tarot.pullingCard();
+
+        System.out.print("Third, ");
+        tarot.pullingCard();
+
+        input.close();
+    }
 }
